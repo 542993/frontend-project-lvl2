ba85960338c2a82dd545432ef0fbdd828b455fb2
Alex <ustus777@mail.rugit>
2021-11-08 13:22:11 +0300

    fix tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -10,25 +10,25 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('test json-stylish', () => {
+test('json-stylish', () => {
   expect(genDiff('file1.json', 'file2.json', 'stylish')).toBe(readFile('result-stylish'));
 });
 
-test('test yml-stylish', () => {
+test('yml-stylish', () => {
   expect(genDiff('filepath1.yml', 'filepath2.yml', 'stylish')).toBe(readFile('result-stylish'));
 });
-test('test json-plain', () => {
+test('json-plain', () => {
   expect(genDiff('file1.json', 'file2.json', 'plain')).toBe(readFile('result-plain'));
 });
 
-test('test yml-plain', () => {
+test('yml-plain', () => {
   expect(genDiff('filepath1.yml', 'filepath2.yml', 'plain')).toBe(readFile('result-plain'));
 });
 
-test('test json-json', () => {
+test('json-json', () => {
   expect(genDiff('file1.json', 'file2.json', 'json')).toBe(readFile('result-json'));
 });
 
-test('test yml-json', () => {
+test('yml-json', () => {
   expect(genDiff('filepath1.yml', 'filepath2.yml', 'json')).toBe(readFile('result-json'));
 });
